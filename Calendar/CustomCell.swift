cdece2f1c9a55fd4acc5d70ead9f9bd5f5e4fc1c
Erik Salas <25646533+eriksalas@users.noreply.github.com>
2019-03-03 11:22:12 -0600

    Create calendar from scratch

---
+++ Calendar/CustomCell.swift
@@ -0,0 +1,14 @@
+//
+//  CustomCell.swift
+//  Calendar
+//
+//  Created by Erik Salas on 3/2/19.
+//  Copyright © 2019 Erik Salas. All rights reserved.
+//
+
+import UIKit
+import JTAppleCalendar
+
+class CustomCell: JTAppleCell {
+    @IBOutlet weak var dateLabel: UILabel!
+}
